fc5fbd8e4ca1b738a2c52d7fbd0c1cdee64b1521
ismayilresus <resulov464@gmail.com>
2021-05-07 13:06:28 +0400

    commit 1

---
+++ app/src/main/java/com/thecheckpoint/composedemo/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.thecheckpoint.composedemo
+
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.thecheckpoint.composedemo.ui.theme.ComposeDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeDemoTheme {
+                }
+            }
+        }
+    }
+}
+
